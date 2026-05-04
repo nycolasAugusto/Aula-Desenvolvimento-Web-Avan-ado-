1801a157aa7e41fee96ac897a8d0cc01959ad655
NYCOLAS AUGUSTO <nycolas.augusto@cs.up.edu.br>
2026-05-04 19:21:45 -0300

    jwt

--- AULA 09 JWT/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-namespace Aula09.Models
-{
-public class Login
-{
-public string Usuario { get; set; }
-public string Senha { get; set; }
-}
-}
--- AULA 09 JWT/Program.cs
+++ AULA 09 JWT/Program.cs
@@ -1,21 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+var key = "minha-chave-super-secreta-com-mais-de-32-caracteres";
 
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
 
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
